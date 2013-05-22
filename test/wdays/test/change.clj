cc5b7128cf85a3e419b91e0e38e44a89443c9f7a
Arkadiy Drovosekov <arkdro@gmail.com>
2013-05-23 00:55:08 +0400

    add test

---
+++ test/wdays/test/change.clj
@@ -0,0 +1,9 @@
+(ns wdays.test.change
+  (:use [wdays.change])
+  (:use [clojure.test]))
+
+(deftest upd-wday-test
+  (let [wd [1.0 2.0 3]
+        new-wday (wdays.change/upd-wday wd)]
+    (is [1 2 3] new-wday)))
+
